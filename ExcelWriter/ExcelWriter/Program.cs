c69d074f080d8023cc9cfd464a747eac3343c070
Teodor Marinov <tvmarinov@gmail.com>
2017-07-18 13:18:06 -0700

    Added two tasks

---
+++ ExcelWriter/ExcelWriter/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace ExcelWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = {"Ivan", "Georgi", "Radoslav", "Mariqna", "Alexandra",
+                              "Hristo", "Peter", "Qvor", "Desislava", "Diana"};
+            int row = 0;
+            int avgScoreRow = 0;
+            int rndScore = 0;
+            float score = 0;
+            float avgScore = 0;
+
+            Random rnd = new Random();
+
+            var excelApp = new Excel.Application();
+            excelApp.Visible = false; // Does not open window for "Excel.exe".
+            excelApp.DisplayAlerts = false;  //Does not ask permision to override, directly does it.
+
+            Excel.Workbook book = excelApp.Workbooks.Add();
+            Excel._Worksheet ws = (Excel.Worksheet)excelApp.ActiveSheet;
+
+            ws.Cells[1, "A"] = "Name";
+            ws.Cells[1, "B"] = "Age";
+            ws.Cells[1, "C"] = "Score";
+            ws.Cells[1, "E"] = "Average Score"; 
+
+            ws.Range[ws.Cells[1, "A"],ws.Cells[1,"E"]].Interior.Color = Excel.XlRgbColor.rgbLightBlue;
+            ws.Range[ws.Cells[1, "A"], ws.Cells[1, "E"]].EntireRow.Font.Bold = true;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                score = 0;
+
+                for (int j = 2; j<=101; j++)  // First row is reserved for headers
+                {
+
+                    row = j + 100 * i;
+                    rndScore = rnd.Next(0, 100);
+                    ws.Cells[row, "A"] = names[rnd.Next(0, 9)];
+                    ws.Cells[row, "B"] = rnd.Next(20, 80);
+                    ws.Cells[row, "C"] = rndScore;
+                    if(j%2 == 1)
+                    {
+                        ws.Range[ws.Cells[row, "A"], ws.Cells[row, "C"]].EntireRow.Font.Color = Excel.XlRgbColor.rgbLightGreen;
+                    }
+
+                    score += rndScore;
+                }
+
+                
+                avgScore = score / 100;
+                avgScoreRow = 2 + (100 * i);
+                ws.Cells[avgScoreRow,"E"] = avgScore;
+            }
+
+            ws.Range[ws.Cells[1, "A"], ws.Cells[1, "E"]].EntireColumn.AutoFit();
+
+            book.SaveAs("c:\\temp\\scores");
+            book.Close();
+
+            //Releasing object incase background excel processes do not close.
+            Marshal.ReleaseComObject(ws); 
+            Marshal.ReleaseComObject(book);
+
+
+            excelApp.Quit();
+
+        }
+    }
+}
---
+++ Factorials/Factorials/Factorials.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorials
+{
+    class Factorials
+    {
+        static void Main(string[] args)
+        {
+            int i,j;
+            long fact = 1;
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            watch.Start();
+
+            for (i = 0; i <= 1000; i++)
+            {
+                for(j = 0; j <= 10; j++)
+                {
+                    fact = Factorials.CalculateFactorialRec(j);
+                    Console.Write(fact + " ");
+                }
+                Console.WriteLine();
+            }
+
+            watch.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+            
+        }
+
+        //public static int CalculateFactorial(int factorial)
+        //{
+        //    int i,result = 1;
+        //    for (i = 0; i <= factorial; i++)
+        //    {
+        //        if (i > 1)
+        //        {
+        //            result *= i;
+        //        }
+        //    }
+
+        //    return result;
+        //}
+
+        public static int CalculateFactorialRec(int factorial)
+        {
+            if (factorial <= 1)
+            {
+                return 1;
+            }
+            return factorial * Factorials.CalculateFactorialRec(factorial - 1);
+        }
+    }
+}
